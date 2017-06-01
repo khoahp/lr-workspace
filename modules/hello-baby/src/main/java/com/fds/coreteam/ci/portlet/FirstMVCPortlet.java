034b543888c767e34f53da7d5064f8e57da70842
Khoa Vu Dang <khoavd@fds.vn>
2017-06-01 17:42:35 +0700

    Refactor code structure

--- modules/hello-baby/src/main/java/com/fds/coreteam/ci/portlet/FirstMVCPortlet.java
+++ modules/hello-baby/src/main/java/com/fds/coreteam/ci/portlet/FirstMVCPortlet.java
@@ -14,7 +14,7 @@ import org.osgi.service.component.annotations.Component;
 	property = {
 		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=hello-baby Portlet",
+		"javax.portlet.display-name=hello-baby portlet",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.resource-bundle=content.Language",
